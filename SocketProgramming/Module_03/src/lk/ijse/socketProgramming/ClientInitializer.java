f01e89ac7787b90790645a6845cef38302758d3e
unknown <pasanpahasara7788@gmail.com>
2022-08-03 15:55:47 +0530

    Added Client View Form

---
+++ SocketProgramming/Module_03/src/lk/ijse/socketProgramming/ClientInitializer.java
@@ -0,0 +1,24 @@
+package lk.ijse.socketProgramming; /**
+ * @author : Pasan Pahasara
+ * @since : 0.1.0
+ **/
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ClientInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("view/ClientForm.fxml"))));
+        primaryStage.show();
+    }
+}

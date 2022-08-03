24604401eb00430184a7ea1d30209f7d8b8473cf
unknown <pasanpahasara7788@gmail.com>
2022-08-03 15:41:44 +0530

    Added Server Controller Form

--- SocketProgramming/Module_03/src/lk/ijse/socketProgramming/ServerInitializer.java
+++ SocketProgramming/Module_03/src/lk/ijse/socketProgramming/ServerInitializer.java
@@ -5,13 +5,10 @@ package lk.ijse.socketProgramming;/**
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
 
 public class ServerInitializer extends Application {
 
---
+++ SocketProgramming/Module_03/src/lk/ijse/socketProgramming/controller/ServerFormController.java
@@ -0,0 +1,22 @@
+package lk.ijse.socketProgramming.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * @author : Pasan Pahasara
+ * @since : 0.1.0
+ **/
+public class ServerFormController {
+    public Button server_side_send_btn;
+    public AnchorPane serverSideAnchorPane;
+    public TextArea textArea;
+    public TextField textMessage;
+
+
+    public void sendOnAction(ActionEvent actionEvent) {
+    }
+}

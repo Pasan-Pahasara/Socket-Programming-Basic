1ce16708dd405caa7265904335e40c550024e41a
unknown <pasanpahasara7788@gmail.com>
2022-08-03 15:47:34 +0530

    Fully Refactor Server Controller Form

--- SocketProgramming/Module_03/src/lk/ijse/socketProgramming/controller/ServerFormController.java
+++ SocketProgramming/Module_03/src/lk/ijse/socketProgramming/controller/ServerFormController.java
@@ -6,6 +6,12 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * @author : Pasan Pahasara
  * @since : 0.1.0
@@ -16,7 +22,38 @@ public class ServerFormController {
     public TextArea textArea;
     public TextField textMessage;
 
+    final int PORT = 5000;
+    ServerSocket serverSocket;
+    Socket accept;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
 
-    public void sendOnAction(ActionEvent actionEvent) {
+    String message = "";
+
+    public void initialize(){
+        new Thread(() -> {
+            try {
+                serverSocket = new ServerSocket(PORT);
+                textArea.appendText("Server Started..");
+                accept = serverSocket.accept();
+                textArea.appendText("\nClient Connected..");
+
+                dataInputStream = new DataInputStream(accept.getInputStream());
+                dataOutputStream = new DataOutputStream(accept.getOutputStream());
+
+                while (!message.equals("exit")){
+                    message = dataInputStream.readUTF();
+                    textArea.appendText("\nClient : " + message);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    public void sendOnAction(ActionEvent actionEvent) throws IOException {
+        dataOutputStream.writeUTF(textMessage.getText().trim());
+        dataOutputStream.flush();
     }
 }

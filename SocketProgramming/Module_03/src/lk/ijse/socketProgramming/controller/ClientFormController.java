aaf68cda5a375c1cd2918907338f905fad79ed7d
unknown <pasanpahasara7788@gmail.com>
2022-08-03 16:03:44 +0530

    Fully Refactor Client Controller Form

--- SocketProgramming/Module_03/src/lk/ijse/socketProgramming/controller/ClientFormController.java
+++ SocketProgramming/Module_03/src/lk/ijse/socketProgramming/controller/ClientFormController.java
@@ -4,6 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 /**
  * @author : Pasan Pahasara
  * @since : 0.1.0
@@ -13,6 +18,34 @@ public class ClientFormController {
     public TextArea textArea;
     public TextField textMessage;
 
-    public void sendOnAction(ActionEvent actionEvent) {
+    final int PORT = 5000;
+    Socket socket;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+
+    String message = "";
+
+    public void initialize(){
+        new Thread(() -> {
+            try {
+                socket = new Socket("localhost",PORT);
+
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+                while (!message.equals("exit")){
+                    message = dataInputStream.readUTF();
+                    textArea.appendText("\n Server : " + message);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    public void sendOnAction(ActionEvent actionEvent) throws IOException {
+        dataOutputStream.writeUTF(textMessage.getText().trim());
+        dataOutputStream.flush();
     }
 }
